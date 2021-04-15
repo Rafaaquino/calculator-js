4eee7a41160a877a388ecd7b55b820121245de25
Rafael Aquino <rafaaquino28@gmail.com>
2021-04-14 21:58:56 -0300

    validation test addition

---
+++ src/js/calculadora.js
@@ -0,0 +1,27 @@
+var Calculadora = {
+	adicionar: function(num1, num2){
+
+		if (isNaN(num1)){
+			return 0
+		}
+		
+		num1 = parseFloat(num1);
+		num2 = parseFloat(num2);
+		return num1 + num2
+	},
+	subtrair: function(num1, num2){
+		num1 = parseFloat(num1);
+		num2 = parseFloat(num2);
+		return num1 - num2
+	},
+	dividir: function(num1, num2){
+		return num1 / num2
+	},
+	multiplicar: function(num1, num2){
+		return num1 * num2
+	},
+};
+// usando na função 'require' do NodeJS
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefinid'){
+	module.exports = Calculadora;
+}
